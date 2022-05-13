8b99bcc2891e44318e00096ece9d7c44aaa8d61e
EDzakharov <divizia21@bk.ru>
2022-05-13 15:43:42 +0300

    v4.3

--- src/Components/CoinPage/CoinPageData/CoinPageData.jsx
+++ src/Components/CoinPage/CoinPageData/CoinPageData.jsx
@@ -1,41 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import style from './CoinPageData.module.scss'
+import {checkEnd} from "../../../Filters";
 
 export const CoinPageData = (props) => {
 
     let timestamp = new Date(props.lastShare * 1000);
     let setEnd = timestamp.getSeconds().toString().slice(-1)
-
-
-    let checkEnd = (setEnd) => {
-        switch (setEnd) {
-            case '0':
-                return 'д'
-            case '1':
-                return 'ду'
-            case '2':
-                return 'ды'
-            case '3':
-                return 'ды'
-            case '4':
-                return 'ды'
-            case '5':
-                return 'д'
-            case '6':
-                return 'д'
-            case '7':
-                return 'д'
-            case '8':
-                return 'д'
-            case '9':
-                return 'д'
-            default:
-                return 'д'
-
-        }
-
-    }
-
 
     let [show, setShow] = useState(false)
 
@@ -43,20 +13,34 @@ export const CoinPageData = (props) => {
     let textCopy = () => {
         navigator.clipboard.writeText(props.miner)
         setShow(!show)
-        setTimeout(()=>{
+        setTimeout(() => {
             setShow(false)
-        },200)
+        }, 200)
     }
 
 
     return (
         <div className={style.grid_data}>
-            <div className={style.miner_s}><div className={style.miner} >{props.miner.toString().length > 60 ? <span className={style.longMiner}>{props.miner}
-            <i className="fa-solid fa-copy" onClick={textCopy}/>{show? <span className={style.copy}> Copy</span>:''}</span> :
-                <span className={style.shortMiner}>{props.miner} <i className="fa-solid fa-copy" onClick={textCopy}/>{show? <span className={style.copy}> Copy</span>:''}</span>}</div></div>
-            <div className={style.hashrate_s}><div className={style.hashrate}><span>{props.hashrate.hashrate}{props.hashrate.unit}</span></div></div>
-            <div className={style.shares_s}><div className={style.shares}><span>{`${timestamp.getSeconds()} секун${checkEnd(setEnd)} назад`}</span></div></div>
-            <div className={style.status_s}><div className={props.offline? style.status_offline : style.status_online}><span>{`${props.offline ? 'Offline':'Online'}`}</span></div></div>
+            <div className={style.miner_s}>
+                <div className={style.miner}>{props.miner.toString().length > 60 ?
+                    <span className={style.longMiner}>{props.miner}
+                        <i className="fa-solid fa-copy" onClick={textCopy}/>{show ?
+                            <span className={style.copy}> Copy</span> : ''}</span> :
+                    <span className={style.shortMiner}>{props.miner} <i className="fa-solid fa-copy"
+                                                                        onClick={textCopy}/>{show ?
+                        <span className={style.copy}> Copy</span> : ''}</span>}</div>
+            </div>
+            <div className={style.hashrate_s}>
+                <div className={style.hashrate}><span>{props.hashrate.hashrate}{props.hashrate.unit}</span></div>
+            </div>
+            <div className={style.shares_s}>
+                <div className={style.shares}><span>{`${timestamp.getSeconds()} секун${checkEnd(setEnd)} назад`}</span>
+                </div>
+            </div>
+            <div className={style.status_s}>
+                <div className={props.offline ? style.status_offline : style.status_online}>
+                    <span>{`${props.offline ? 'Offline' : 'Online'}`}</span></div>
+            </div>
 
         </div>
 
